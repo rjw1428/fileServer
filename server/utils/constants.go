e4c08dd7efa2e240e12c7c4597ed582de8844e1b
Ryan Wilk <rjw1428@gmail.com>
2023-02-11 19:16:23 -0500

    changing ROOT_DIR

--- server/utils/constants.go
+++ server/utils/constants.go
@@ -1,8 +1,8 @@
 package utils
 
-const ROOT_DIR = "C:/Users/rjw14/Documents/"
+// const ROOT_DIR = "C:/Users/rjw14/Documents/"
 
-// const ROOT_DIR = "/media/pi/Crucial X6/"
+const ROOT_DIR = "/media/pi/Crucial X6/"
 const API_VERSION = "v1"
 const PORT = 1428
 const MAX_UPLOAD_SIZE = 1024 * 1024 * 1024 * 10
